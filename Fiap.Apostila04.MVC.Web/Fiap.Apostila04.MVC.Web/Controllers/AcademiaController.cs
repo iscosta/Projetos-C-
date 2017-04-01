4d3e910f6aed735cdd534b45004aacb7711d2d9d
Igor Costa <igor.scosta21@gmail.com>
2017-04-01 14:44:23 -0300

    Add files via upload

---
+++ Fiap.Apostila04.MVC.Web/Fiap.Apostila04.MVC.Web/Controllers/AcademiaController.cs
@@ -0,0 +1,43 @@
+﻿using Fiap.Apostila04.MVC.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Apostila04.MVC.Web.Controllers
+{
+    public class AcademiaController : Controller
+    {
+        //Simular o banco de dados
+        private static List<Academia> _lista = new List<Academia>();
+
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            //Passar a lista de planos para a tela (select)
+            List<string> planos = new List<string>();
+            planos.Add("Ouro");
+            planos.Add("Prata");
+            planos.Add("Bronze");
+            ViewBag.opcoes = new SelectList(planos);
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Academia academia)
+        {
+            _lista.Add(academia); //Adiciona a academia na lista
+            TempData["mensagem"] = "Academia cadastrada.";
+            return Redirect("Cadastrar"); //Redireciona para o cadastrar
+        }
+
+        [HttpGet]
+        public ActionResult Listar()
+        {
+            //ViewBag.samurai = _lista;
+            //Passa a lista para a tela
+            return View(_lista);
+        }
+    }
+}
---
+++ Fiap.Apostila04.MVC.Web/Fiap.Apostila04.MVC.Web/Models/Academia.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Fiap.Apostila04.MVC.Web.Models
+{
+    public class Academia
+    {
+        public string Cnpj { get; set; }
+        public string Nome { get; set; }
+
+        [Display(Name ="Número de Alunos")]
+        public int NumeroAlunos { get; set; }
+
+        public bool Piscina { get; set; }
+
+        [Display(Name = "Data de Inscrição")]
+        public DateTime DataInscricao { get; set; }
+        public string Plano { get; set; }
+    }
+}
