ea39aa33814319512917d02b10e4dbef41ccc4e3
Igor Costa <igor.scosta21@gmail.com>
2017-04-01 14:40:19 -0300

    Add files via upload

---
+++ Fiap.Exercicio02.MVC/Fiap.Exercicio02.MVC.Web/Controllers/UsuarioController.cs
@@ -0,0 +1,25 @@
+﻿using Fiap.Exercicio02.MVC.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Exercicio02.MVC.Web.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        [HttpGet] //Abre a página
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Usuario usuario)
+        {
+            TempData["msg"] = "Cadastrado!";
+            return View("Resultado",usuario);//Abre a página Resultado.cshtml
+        }
+    }
+}
